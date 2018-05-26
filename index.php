20b449762410e99407ac76e300a9d40664ec1809
Gary Nye <frosty985@googlemail.com>
2018-05-26 11:50:03 +0100

    Scripted auto-commit on change (2018-05-26 11:50:03) by gitwatch.sh

--- index.php
+++ index.php
@@ -4,3 +4,11 @@ require_once("config.php");
 require_once("header.php");
 
 ?>
+
+<div name="page">
+  <div name="header">
+    Welcome <?php echo "$user[fname]"; ?>.
+    <nav>
+    </nav>
+  </div>
+</div>
