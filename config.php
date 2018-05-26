616f7dddf07fda4033a7eba289eaa7bf32ccf834
Gary Nye <frosty985@googlemail.com>
2018-05-26 11:47:31 +0100

    Scripted auto-commit on change (2018-05-26 11:47:31) by gitwatch.sh

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $MySQL["data"] = "time_card";
 $db = mysqli_connect($MySQL["host"], $MySQL["user"], $MySQL["pass"], $MySQL["data"]);
 
 $debug = "on";
-
+$uid = "0a25bf3160d211e899675254004146e6";
 
 if (!$db)
 {
