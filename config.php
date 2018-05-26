59ae528c0f327df814f6efdc5cc77496cb275cd8
Gary Nye <frosty985@googlemail.com>
2018-05-26 11:35:54 +0100

    Scripted auto-commit on change (2018-05-26 11:35:54) by gitwatch.sh

--- config.php
+++ config.php
@@ -1,3 +1,19 @@
 <?php
 
+$MySQL["user"] = "time_card";
+$MySQL["pass"] = "T1me_C@rd";
+$MySQL["host"] = "localhost";
+$MySQL["data"] = "time_card";
+
+$db = mysqli_connect($MySQL["host"], $MySQL["user"], $MySQL["pass"], $MySQL["data"]);
+
+if (!$db)
+{
+  echo "Error connecting to MySQL." . PHP_EOL;
+  echo "Debug Error No: " . mysqli_connect_errno() . PHP_EOL;
+  echo "Debug Error: " . mysqli_connect_error() . PHP_EOL;
+
+  exit();
+}
+
 ?>
