39d645f0442cac9746a1c44be67c3d312fd0f6ec
Gary Nye <frosty985@googlemail.com>
2018-05-26 11:24:44 +0100

    Scripted auto-commit on change (2018-05-26 11:24:44) by gitwatch.sh

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>Time Card</title>
+    <link rel="stylesheet" type="text/css" href="layout.css">
+    <link rel="stylesheet" type="text/css" href="fonts.css">
+    <link rel="stylesheer" type="text/css" href="colors.css">
+  </head>
+
+  <body>
