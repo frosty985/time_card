3dd72244ddd0628a16b1f6ab46001ee1c50cac74
Gary Nye <frosty985@googlemail.com>
2018-05-27 10:17:30 +0100

    Scripted auto-commit on change (2018-05-27 10:17:30) by gitwatch.sh

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
 require_once("config.php");
 require_once("header.php");
 
+if (!isset($_GET["start_date"]))
+{
+  $start_date = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+}
+else
+{
+  $start_date = $_GET["start_date"];
+}
+
 ?>
 
 <div name="page">
@@ -26,11 +35,13 @@ echo "\t\t<div class=\"dTable\">\n";
 echo "\t\t\t<div class=\"dTableBody\">\n";
 
 echo "\t\t\t\t<div class=\"dTableRow\">\n";
-echo "\t\t\t\t\t<div class=\"dTableHeading\">Day</div>\n";
-echo "\t\t\t\t\t<div class=\"dTableHeading\">Start</div>\n";
-echo "\t\t\t\t\t<div class=\"dTableHeading\">Break Start</div>\n";
-echo "\t\t\t\t\t<div class=\"dTableHeading\">Break Finsih</div>\n";
-echo "\t\t\t\t\t<div class=\"dTableHeading\">Finish</div>\n";
+echo "\t\t\t\t\t<div class=\"dTableHeadCell\">Day</div>\n";
+echo "\t\t\t\t\t<div class=\"dTableHeadCell\">Type</div>\n";
+echo "\t\t\t\t\t<div class=\"dTableHeadCell\">Start</div>\n";
+echo "\t\t\t\t\t<div class=\"dTableHeadCell\">Finsih</div>\n";
+echo "\t\t\t\t\t<div class=\"dTableHeadCell\">Time</div>\n";
+echo "\t\t\t\t\t<div class=\"dTableHeadCell\">Save</div>\n";
+echo "\t\t\t\t\t<div class=\"dTableHeadCell\">Delete</div>\n";
 echo "\t\t\t\t</div>\n";
 
 for ($d = 0; $d < 7; $d++)
@@ -38,23 +49,25 @@ for ($d = 0; $d < 7; $d++)
   /// create a "date" based on differance of days of today and loop number
   if ($d < date("w"))
   {
-    $mkd = mktime(0, 0, 0, date("m"), date("d")+$d-date("w"), date("Y"));
+    $mkd = mktime(0, 0, 0, date("m", $start_date), date("d", $start_date)+$d-date("w", $start_date), date("Y", $start_date));
   }
   elseif ($d > date("w"))
   {
-    $mkd = mktime(0, 0, 0, date("m"), date("d")-$d+date("w"), date("Y"));
+    $mkd = mktime(0, 0, 0, date("m", $start_date), date("d", $start_date)-$d+date("w", $start_date), date("Y", $start_date));
   }
   else
   {
-    $mkd = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+    $mkd = mktime(0, 0, 0, date("m", $start_date), date("d", $start_date), date("Y", $start_date));
   }
 
   echo "\t\t\t\t<div class=\"dTableRow\">\n";
   echo "\t\t\t\t\t<div class=\"dTableCell\">" . date("l", $mkd) . "</div>\n";
-  echo "\t\t\t\t\t<div class=\"dTableHeading\"><input name=\"start_$d\"></div>\n";
-  echo "\t\t\t\t\t<div class=\"dTableHeading\"><input name=\"bstart_$d\"></div>\n";
-  echo "\t\t\t\t\t<div class=\"dTableHeading\"><input name=\"bfinish_$d\"></div>\n";
-  echo "\t\t\t\t\t<div class=\"dTableHeading\"><input name=\"finish_$d\"></div>\n";
+  echo "\t\t\t\t\t<div class=\"dTableCell\"><input name=\"type_$d\"></div>\n";
+  echo "\t\t\t\t\t<div class=\"dTableCell\"><input name=\"start_$d\"></div>\n";
+  echo "\t\t\t\t\t<div class=\"dTableCell\"><input name=\"finish_$d\"></div>\n";
+  echo "\t\t\t\t\t<div class=\"dTableCell\"><input name=\"time_$d\"></div>\n";
+  echo "\t\t\t\t\t<div class=\"dTableCell\"><input type=\"Submit\" value=\"Save\"></div>\n";
+  echo "\t\t\t\t\t<div class=\"dTableCell\"><a href=\"#\">X</a>\n";
   echo "\t\t\t\t</div>";
 
 
