43b7dce6a48dbe1a4b442a83ba6937871a48c8e3
frosty985 <35139571+frosty985@users.noreply.github.com>
2018-05-29 10:23:31 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,10 +1,10 @@
 <?php
 require_once("config.php");
 
-foreach($_POST as $Key => $val)
-{
-  echo "$Key -> $val<br>\n";
-}
+//foreach($_POST as $Key => $val)
+//{
+//  echo "$Key -> $val<br>\n";
+//}
 
 if (isset($_POST["update"])) 
 {
@@ -14,13 +14,12 @@ if (isset($_POST["update"]))
 if (isset($_POST["delete"]))
 {
   // delete from database
-  
   $update_sql = "DELETE FROM time WHERE tid = \"$_POST[tid]\"; ";
 }
 
 mysqli_query($db, $update_sql);
-echo $update_sql;
+//echo $update_sql;
 
-//header("Location: index.php");
+header("Location: index.php");
 
 ?>
