76934c8fbe5e8927c2c0f020799228fc3795b190
Gary Nye <frosty985@googlemail.com>
2018-05-29 21:02:23 +0100

    Scripted auto-commit on change (2018-05-29 21:02:23) by gitwatch.sh

--- login.php
+++ login.php
@@ -4,19 +4,21 @@ require_once("config.php");
 require_once("header.php");
 session_start();
 
+if ($login)
+{
+  header("Location: ". $_GET["ref"]);
+}
+
+
 ?>
 
 <div class="login">
   You must be logged in to access this site.
-  <label for="uName">User Name:</lable>
-  <input name="uName" placeholder="Username" required />
-  <label for="pWord">Password:</lable>
-  <input name="pWord" required />
-  <input type="submit" name="login" value="Log in" />
+  <form class="fLogin" action="login.php" method="post">
+    <label for="uName">User Name:</label>
+    <input name="uName" placeholder="Username" required />
+    <label for="pWord">Password:</label>
+    <input type="password" name="pWord" required />
+    <input type="submit" name="login" value="Log in" />
+  </form>
 </div>
-
-<?php
-
-//header("Location: ". $_GET["ref"]);
-
-?>
