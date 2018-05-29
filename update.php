57a4251fa0c312fbaf3517ed4aa191d84f22356c
frosty985 <35139571+frosty985@users.noreply.github.com>
2018-05-29 09:57:45 +0100

    Create update.php
    to update database

---
+++ update.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST["update"])) 
+{
+  // update data base
+}
+
+if (isset($_POST["delete"]))
+{
+  // delete from database
+  
+  $update_sql = "DELETE FROM time WHERE tid = \"$_POST[tid]\"; ";
+}
+
+mysqli_query($db, $update_sql);
+
+header(Location: "index.php");
+
+?>
