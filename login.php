d648e7b3d55ea8a326e05f313b43fee4b0dc61b1
Gary Nye <frosty985@googlemail.com>
2018-05-29 20:59:09 +0100

    Scripted auto-commit on change (2018-05-29 20:59:09) by gitwatch.sh

--- login.php
+++ login.php
@@ -4,8 +4,19 @@ require_once("config.php");
 require_once("header.php");
 session_start();
 
+?>
 
-header("Location: ". $_GET["ref"]);
+<div class="login">
+  You must be logged in to access this site.
+  <label for="uName">User Name:</lable>
+  <input name="uName" placeholder="Username" required />
+  <label for="pWord">Password:</lable>
+  <input name="pWord" required />
+  <input type="submit" name="login" value="Log in" />
+</div>
 
+<?php
+
+//header("Location: ". $_GET["ref"]);
 
 ?>
