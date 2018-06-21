b20b395030951c719336e308b8ba6c81518cbb62
Gary <frosty985@googlemail.com>
2018-06-21 22:24:13 +0100

    change layout

--- header.php
+++ header.php
@@ -1,10 +1,6 @@
 <?php
 require_once("config.php");
 
-$user_sql = "SELECT uname, fname, lname FROM user WHERE uid='$uid'";
-$user_query = mysqli_query($db, $user_sql);
-$user = mysqli_fetch_array($user_query);
-
 ?>
 
 <html>
@@ -13,7 +9,63 @@ $user = mysqli_fetch_array($user_query);
     <link rel="stylesheet" type="text/css" href="layout.css">
     <link rel="stylesheet" type="text/css" href="fonts.css">
     <link rel="stylesheet" type="text/css" href="colors.css">
-  </head>
+    <script>
+      function calc_time(input) {
+        // get row number
+        var row = input.id.substring(input.id.length-1, input.id.length)
+        // var inType = inp.name.substring(0, inp.name.length-2)
 
+        // create vars for time
+        var starttime = document.getElementById('start_' + row).value
+        var stoptime = document.getElementById('finish_' + row).value
+
+        // build date
+        var startdate = new Date("01/01/2001 " + starttime)
+        var stopdate = new Date("01/01/2001 " + stoptime)
+
+        // calculate differance
+        var hours = stopdate - startdate
+        //alert(hours)
+
+        // show differance
+        document.getElementById('time_' + row).value = new Date(hours).toISOString().substr(11,8)
+      }
+
+      function valid_time(input) {
+        re = /^(\d{1,2}):(\d{2})/;
+        if (input.value != "")
+        {
+          if (regs = input.value.match(re))
+          {
+            if (regs[1] < 23)
+            {
+              if (regs[2] > 59)
+              {
+                alert("Invalid time");
+                return false;
+              }
+            }
+            else
+            {
+              alert("Invalid time");
+              return false;
+            }
+          }
+          return true;
+        }
+        alert("Invalid time");
+        return false;
+      }
+
+      function valid_form(fInput) {
+        if (fInput.type.value != "Holiday" || fInput.type.value != "Bank Holiday")
+        {
+          if (!valid_time(fInput.start)) return false;
+          if (!valid_time(fInput.finish)) return false;
+        return true;
+        }
+      }
+    </script>
+  </head>
   <body>
-<div name="page">
+    <div name="page">
--- nav.php
+++ nav.php
@@ -1,29 +1,30 @@
-<?php
-  echo "<div name=\"header\" class=\"header\">\n";
-  echo "<h3>Welcome $user[fname],";
+    <div name="header" class="header">
+      <h3>Welcome <?php echo "$_SESSION[fname]" . ",";
 
-  $comp_sql = "SELECT cname FROM user_comp JOIN company ON company.cid = user_comp.cid WHERE uid = \"$_SESSION[uid]\" ORDER BY edate DESC LIMIT 1;";
+  
+  $comp_sql = "SELECT cname, company.cid FROM user_comp JOIN company ON company.cid = user_comp.cid WHERE uid = \"$_SESSION[uid]\" ORDER BY edate DESC LIMIT 1;";
   $comp_query = mysqli_query($db, $comp_sql);
-
-  if (mysqli_num_rows($comp_query) != 0)
-  {
+  if (mysqli_num_rows($comp_query) != 0) {
     $comp = mysqli_fetch_array($comp_query);
-    echo " viewing time card for $comp[cname]\n";
+    $_SESSION["cid"] = $comp["cid"];
+    echo " viewing time card for $comp[cname]</h3>\n";
   }
-  echo "</h3>\n";
-  echo "<nav>\n";
-  echo "| <a href=\"index.php\" class=\"button button-medium button-primary\">Home</a> |\n";
+
+  echo "      <nav>\n";
+  echo "        <ul>\n";
+  echo "          <li><a href=\"index.php\" class=\"button button-medium button-primary\">Home</a></li>\n";
 
   if (mysqli_num_rows($comp_query) != 0)
   {
-    echo " <a href=\"company.php\" class=\"button button-medium button-primary\">Adjust company details</a> |\n";
+    echo "          <li><a href=\"company.php\" class=\"button button-medium button-primary\">Adjust company details</a></li>\n";
   }
   else
   {
-    echo " <a href=\"company.php\" class=\"button button-medium button-primary\">Add a company</a> |\n";
+    echo "          <li><a href=\"company.php\" class=\"button button-medium button-primary\">Add a company</a></li>\n";
   }
-  echo " <a href=\"reports.php\" class=\"button button-medium button-primary\">View reports</a> |\n";
-  echo " <a href=\"logout.php\" class=\"button button-medium button-primary\">Logout</a> |\n";
 ?>
-    </nav>
-  </div>
+          <li><a href="reports.php" class="button button-medium button-primary">View reports</a></li>
+          <li><a href="logout.php" class="button button-medium button-primary">Logout</a></li>
+        </ul>
+      </nav>
+    </div>
